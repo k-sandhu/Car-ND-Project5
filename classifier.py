882165ebb5c2aecdb60bfdf9f372c0cf0174d3e7
Kamal <sandhu.kamal@live.com>
2017-11-15 14:56:30 -0700

    update knobs

--- classifier.py
+++ classifier.py
@@ -12,7 +12,40 @@ from sklearn.preprocessing import StandardScaler
 from skimage.feature import hog
 from sklearn.feature_selection import SelectFromModel
 from sklearn.pipeline import Pipeline
+import pandas as pd
 
+def get_data():
+    # Divide up into cars and notcars
+    images = glob.glob('./data/train/**/*.png', recursive=True)
+    cars = []
+    notcars = []
+    for image in images:
+        if 'non-vehicles' in image:
+            notcars.append(image)
+        else:
+            cars.append(image)
+
+    if sample_size > 0:
+        cars = cars[0:sample_size]
+        notcars = notcars[0:sample_size]
+
+    return cars, notcars
+
+def get_features(cars, notcars, cspace, orient, pix_per_cell, cell_per_block, hog_channel):
+
+    car_features = extract_features(cars, cspace=cspace, orient=orient,
+                                    pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                    hog_channel=hog_channel)
+    notcar_features = extract_features(notcars, cspace=cspace, orient=orient,
+                                       pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel)
+
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)  # Create an array stack of feature vectors
+    X_scaler = StandardScaler().fit(X)  # Fit a per-column scaler
+    scaled_X = X_scaler.transform(X)  # Apply the scaler to X
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))  # Define the labels vector
+
+    return X, scaled_X, y
 
 def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                         vis=False, feature_vec=True):
@@ -70,106 +103,159 @@ def extract_features(imgs, cspace='RGB', orient=9,
     # Return list of feature vectors
     return features
 
-def train_model(svc_base, X_train, y_train):
+def train_model(svc_base, X_train, y_train, cv, n_calls, n_random_starts, n_points):
     def svc_base_objective(params):
-        C, tol, max_iter = params
+        C, tol= params
 
-        svc_base.set_params(C=C, tol=tol, max_iter=max_iter)
-        return -np.mean(cross_val_score(svc_base, X_train, y_train, cv=25, n_jobs=-1, scoring="accuracy", verbose=1))
+        svc_base.set_params(C=C, tol=tol)
+        return -np.mean(cross_val_score(svc_base, X_train, y_train, cv=cv, n_jobs=-1, scoring="accuracy", verbose=1))
 
     svc_space = [(1,6),            # C
-                    (0.0001, 0.001),           # tol
-                    (1000, 5000)]           # max_iter
+                    (0.0001, 0.001)]          # tol
 
     svc_opt = gp_minimize(svc_base_objective, svc_space,
-                          n_calls=8, n_random_starts=3, acq_func='gp_hedge',
+                          n_calls=n_calls, n_random_starts=n_random_starts, acq_func='gp_hedge',
                           acq_optimizer='lbfgs', x0=None, y0=None, random_state=None,
-                          verbose=True, callback=None, n_points=100,
+                          verbose=True, callback=None, n_points=n_points,
                           n_restarts_optimizer=5, xi=0.01, kappa=1.96,
                           noise='gaussian', n_jobs=-1)
     #joblib.dump(svc_opt, "svc_opt.pkl")
     return svc_opt.x
 
 if __name__ == "__main__":
-    # Divide up into cars and notcars
-    images = glob.glob('./data/train/**/*.png',recursive=True)
-    cars = []
-    notcars = []
-    for image in images:
-        if 'non-vehicles' in image:
-            notcars.append(image)
-        else:
-            cars.append(image)
-
     # Reduce the sample size because HOG features are slow to compute
     # The quiz evaluator times out after 13s of CPU time
     sample_size = 500
-    cars = cars[0:sample_size]
-    notcars = notcars[0:sample_size]
 
-    ### TODO: Tweak these parameters and see how the results change.
-    colorspace = 'RGB' #['RGB', 'HSV', 'LUV', 'HLS', 'YUV', 'YCrCb'] # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    n_calls = 2
+    n_random_starts = 1
+    n_points = 100
+    cv=5
+
+    colorspace_options = ['RGB', 'HSV', 'LUV', 'HLS', 'YUV', 'YCrCb']
+    hog_channel_options = [0, 1, 2, 'ALL']
+
     orient = 9
     pix_per_cell = 8
     cell_per_block = 4
-    hog_channel = 0 # Can be 0, 1, 2, or "ALL"
 
-    t=time.time()
-    car_features = extract_features(cars, cspace=colorspace, orient=orient,
-                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                            hog_channel=hog_channel)
-    notcar_features = extract_features(notcars, cspace=colorspace, orient=orient,
-                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                            hog_channel=hog_channel)
-    t2 = time.time()
-    print(round(t2-t, 2), 'Seconds to extract HOG features...')
+    cars, notcars = get_data()
 
-    # Create an array stack of feature vectors
-    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+    metrics_dict = ['index', 'cspace', 'hog_channel', 'time_hog', 'f_vec_len',
+                                    'time_train_base', 'time_predict_base', 'score_base',
+                                    'time_train_base_f_sel', 'time_predict_base_f_sel', 'score_base_f_sel', 'feature_mask', 'num_features', 'bopt_param'
+                                    'time_train_f_sel_bopt', 'time_predict_f_sel_bopt', 'score_f_sel_bopt',
+                                    'time_train_cv', 'time_predict_cv', 'score_f_sel_bopt_cv']
 
-    # Fit a per-column scaler
-    X_scaler = StandardScaler().fit(X)
+    metrics_dict = {k:0 for k in metrics_dict}
+    metrics = pd.DataFrame(data=metrics_dict, index=[0])
 
-    # Apply the scaler to X
-    scaled_X = X_scaler.transform(X)
+    count = 0
+    for cspace in colorspace_options:
+        for hog_channel in hog_channel_options:
+            if count == 1:
+                break
+            metrics_dict['index'] = count+1
+            metrics_dict['cspace'] = cspace
+            metrics_dict['hog_channel'] = hog_channel
 
-    # Define the labels vector
-    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+            t=time.time()
+            X, scaled_X, y = get_features(cars, notcars, cspace, orient, pix_per_cell, cell_per_block, hog_channel)
+            t2 = time.time()
+            time_hog = round(t2 - t, 2)
+            print(time_hog, 'Seconds to extract HOG features...')
+            metrics_dict['time_hog'] = time_hog
 
-    # Split up data into randomized training and test sets
-    rand_state = np.random.randint(0, 100)
-    X_train, X_test, y_train, y_test = train_test_split(
-        scaled_X, y, test_size=0.2, random_state=rand_state)
+            X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2) # Split up data into randomized training and test sets
 
-    print('Using:',orient,'orientations',pix_per_cell,
-        'pixels per cell and', cell_per_block,'cells per block')
-    print('Feature vector length:', len(X_train[0]))
+            f_vec_len = len(X_train[0])
+            print('Feature vector length:', f_vec_len)
+            metrics_dict['f_vec_len'] = f_vec_len
 
-    X_scaler = StandardScaler()
-    svc = LinearSVC()
-    sfm = SelectFromModel(svc)
-    X_train_reduced = sfm.fit_transform(X_train, y_train)
-    svc.C, svc.tol, svc.max_iter = train_model(svc, X_train, y_train)
 
-    estimators = [('StandardScaler', X_scaler), ('SelectFromModel', sfm), ('LinearSVC', svc)]
-    pipe = Pipeline(estimators)
+            ######## Train and time base model ##################################
+            t = time.time()
+            svc = LinearSVC()
+            svc.fit(X_train, y_train)
+            t2 = time.time()
+            time_train_base = round(t2 - t, 2)
+            metrics_dict['time_train_base'] = time_train_base
 
-    # Check the training time for the SVC
-    t=time.time()
-    params = {}
-    grid = GridSearchCV(svc, params, cv=25, verbose=1,scoring='accuracy')
-    grid.fit(X, y)
-    t2 = time.time()
+            t = time.time()
+            score = svc.score(X_test,y_test)
+            t2 = time.time()
+            time_predict_base = round(t2 - t, 2)
+            metrics_dict['time_predict_base'] = time_predict_base
 
-    #joblib.dump(grid, 'svc_grid.pkl')
+            metrics_dict['score_base'] = round(score, 6)
+            print('Base Linear SVC score is:', score)
 
-    print(round(t2-t, 2), 'Seconds to train SVC...')
-    # Check the score of the SVC
-    print('Test Accuracy of SVC = ', round(grid.score(X_test, y_test), 4))
-    # Check the prediction time for a single sample
-    t=time.time()
-    n_predict = 10
-    print('My SVC predicts: ', grid.predict(X_test[0:n_predict]))
-    print('For these',n_predict, 'labels: ', y_test[0:n_predict])
-    t2 = time.time()
-    print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+            ######## Train and time base after feature selection ##################################
+            X_scaler = StandardScaler()
+            svc = LinearSVC()
+            sfm = SelectFromModel(svc)
+            t = time.time()
+            X_train_reduced = sfm.fit_transform(X_train, y_train)
+            svc.fit(X_train_reduced, y_train)
+            t2 = time.time()
+            time_train_base_f_sel = round(t2 - t, 2)
+            metrics_dict['time_train_base_f_sel'] = time_train_base_f_sel
+
+            t = time.time()
+            score = svc.score(sfm.transform(X_test), y_test)
+            t2 = time.time()
+            time_predict_base_f_sel = round(t2 - t, 2)
+            metrics_dict['time_predict_base_f_sel'] = time_predict_base_f_sel
+
+            feature_mask = sfm.get_support()
+            metrics_dict['score_base_f_sel'] = round(score, 6)
+            metrics_dict['feature_mask_base'] = feature_mask
+
+            num_features = X_train_reduced.shape[1]
+            metrics_dict['num_features'] = num_features
+
+            print('Number of features selected were: ', num_features)
+            print('Base Linear SVC score after f selection is:', score)
+
+            ######## Train and time bopt after feature selection ##################################
+            t = time.time()
+            svc.C, svc.tol = train_model(svc, X_train_reduced, y_train, cv, n_calls, n_random_starts, n_points)
+            metrics_dict['bopt_param'] = [svc.C, svc.tol]
+            svc.fit(X_train_reduced, y_train)
+            t2 = time.time()
+            time_train_bopt = round(t2 - t, 2)
+            metrics_dict['time_train_bopt'] = time_train_bopt
+
+            t = time.time()
+            score = svc.score(sfm.transform(X_test), y_test)
+            t2 = time.time()
+            time_predict_f_sel_bopt = round(t2 - t, 2)
+            metrics_dict['time_predict_f_sel_bopt'] = time_predict_f_sel_bopt
+            metrics_dict['score_f_sel_bopt'] = round(score, 6)
+
+            ################# cv after bopt ########################
+            t = time.time()
+            params = {}
+            grid = GridSearchCV(svc, params, cv=cv, verbose=1, scoring='accuracy')
+            grid.fit(X_train_reduced, y_train)
+            t2 = time.time()
+            time_train_cv = round(t2 - t, 2)
+            metrics_dict['time_train_cv'] = time_train_cv
+
+            t = time.time()
+            score = grid.score(sfm.transform(X_test), y_test)
+            t2 = time.time()
+            time_predict_cv = round(t2 - t, 2)
+            metrics_dict['time_predict_cv'] = time_predict_cv
+
+            metrics_dict['score_f_sel_bopt_cv'] = round(score, 6)
+
+            print(metrics_dict)
+            metrics = metrics.append(metrics_dict, ignore_index=True)
+            print(metrics)
+            metrics.to_csv("metrics.csv", columns=metrics_dict.keys())
+
+            count+=1
+
+            #estimators = [('StandardScaler', X_scaler), ('SelectFromModel', sfm), ('LinearSVC', svc)]
+            #pipe = Pipeline(estimators)
